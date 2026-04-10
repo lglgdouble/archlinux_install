7fc6b4cf59c094c398d9ad1493f0a404d1103f3e
cyc_archlinux_dev <cyc_archlinux_dev>
2026-04-10 21:13:59 +0800

    xx

---
+++ archlinux_init.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+cdpwd=$(dirname "$(readlink -f "$0")")
+cd "${cdpwd}" || exit 1
+
+#
+#
+#
+cd
+
+#
+#
+#
+export github_proxy="https://ghfast.top"
+export dockerhub_proxy="https://docker.m.daocloud.io"
+
+#
+#
+#
+echo 'Server = http://mirrors.aliyun.com/archlinux/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist
+sudo sed -i '/archlinuxcn/d' /etc/pacman.conf
+echo -e '[archlinuxcn]\nServer = https://mirrors.aliyun.com/archlinuxcn/$arch' | sudo tee -a /etc/pacman.conf
+
+#
+#
+#
+#
+#pip
+mkdir -p ~/.pip/ && cat <<'EOF' | tee ~/.pip/pip.conf >/dev/null
+[global]
+index-url = http://mirrors.aliyun.com/pypi/simple/
+
+[install]
+trusted-host=mirrors.aliyun.com
+EOF
+
+
+#
+#
+#
+#git
+cat <<EOF | tee ~/.gitconfig >/dev/null
+[user]
+	email = cyc_archlinux_dev
+	name = cyc_archlinux_dev
+[init]
+	defaultBranch = main
+[url "https://github.com/lglgdouble/"]
+	insteadOf = https://github.com/lglgdouble/
+[url "${github_proxy}/https://github.com/"]
+	insteadOf = https://github.com/
+EOF
+
+#
+#
+#
+sudo pacman -Syu --noconfirm --needed networkmanager openssh
+sudo systemctl enable NetworkManager
+sudo systemctl enable sshd
+sudo nmcli device status
+sudo nmcli connection modify "Wired connection 1" \
+  ipv4.method manual \
+  ipv4.addresses "192.168.1.100/24" \
+  ipv4.gateway "192.168.1.1" \
+  ipv4.dns "192.168.1.1"
+sudo systemctl restart NetworkManager
+sudo systemctl restart sshd
+
+
+##
+##
+##
+#
+#
+sudo pacman -Sy --noconfirm --needed archlinuxcn-keyring 
+sudo pacman -Sy --noconfirm --needed \
+	paru sudo lsof dos2unix base-devel bash-completion \
+  git \
+  python python-pip python-pipx \
+  
+
+#
+#
+#
+#
+#https://github.com/ultrafunkamsterdam/nodriver
+paru -Sy --noconfirm --needed \
+  google-chrome
+sudo pacman -Sy --noconfirm --needed xorg-server-xvfb
---
+++ archlinux_install.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+cdpwd=$(dirname "$(readlink -f "$0")")
+cd "${cdpwd}" || exit 1
+
+#
+#
+#如果是虚拟机把固件类型设为uefi
+#
+#
+#
+cd
+
+#
+#
+#
+read -p "确认继续？(y/n): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+    echo "执行..."
+else
+    echo "取消操作"
+    exit 3
+fi
+
+#
+#
+#
+echo 'Server = http://mirrors.aliyun.com/archlinux/$repo/os/$arch' >/etc/pacman.d/mirrorlist
+sed -i '/archlinuxcn/d' /etc/pacman.conf
+echo -e '[archlinuxcn]\nServer = https://mirrors.aliyun.com/archlinuxcn/$arch' >>/etc/pacman.conf
+
+#
+#
+#这里要改变disk
+disk="/dev/sda"
+efi="${disk}1"
+swap="${disk}2"
+root="${disk}3"
+
+echo 'label: gpt' | sfdisk ${disk}
+cat << EOF | sfdisk ${disk}
+label: gpt
+${efi} : size=2G, type=U, name=efi
+${swap} : size=12G, type=S, name=swap
+${root} : type=L, name=root
+EOF
+
+mkfs.ext4 -F ${root}
+mkswap ${swap}
+mkfs.fat -F 32 ${efi}
+
+mount --mkdir ${root} /mnt
+mount --mkdir ${efi} /mnt/boot
+swapon ${swap}
+
+pacstrap -K /mnt base linux linux-firmware || exit 110
+genfstab -U /mnt >> /mnt/etc/fstab
+
+
+#
+#
+#
+#
+cat <<'EOF' | tee ./install_arch_chroot.sh >/dev/null
+#!/bin/bash
+cdpwd=$(dirname "$(readlink -f "$0")")
+cd "${cdpwd}" || exit 1
+
+cd
+
+echo 'Server = http://mirrors.aliyun.com/archlinux/$repo/os/$arch' >/etc/pacman.d/mirrorlist
+sed -i '/archlinuxcn/d' /etc/pacman.conf
+echo -e '[archlinuxcn]\nServer = https://mirrors.aliyun.com/archlinuxcn/$arch' >>/etc/pacman.conf
+
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+hwclock --systohc
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/; s/^#zh_CN.UTF-8 UTF-8/zh_CN.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' >/etc/locale.conf
+echo 'yonan' >/etc/hostname
+echo 'root' | passwd -s
+
+pacman -Syu --noconfirm --needed sudo grub efibootmgr os-prober networkmanager openssh
+systemctl enable NetworkManager
+systemctl enable sshd
+
+
+echo '%wheel ALL=(ALL:ALL) ALL' >/etc/sudoers.d/wheel && chmod $(stat -c "%a" /etc/sudoers) /etc/sudoers.d/wheel
+useradd -m -s /bin/bash -G wheel cyc
+echo 'cyc' | passwd -s cyc
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
+echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+EOF
+chmod u+x ./install_arch_chroot.sh
+cp ./install_arch_chroot.sh /mnt/root/
+
+#
+#
+#
+arch-chroot /mnt
+reboot
